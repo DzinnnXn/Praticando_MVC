843b1a3a30fdd88635eec2c5b936efcdcfbbf51b
DzinnnXn <andresilvax444@gmail.com>
2023-10-07 08:37:18 -0300

    up

--- Controller.py
+++ Controller.py
@@ -1,47 +1,45 @@
-from Model import*
+from Model import *
 
 class Controlleradicionartarefa():
     def __init__(self, tarefa):
         self.tarefa = tarefa
+
+    def adicionar_tarefa(self):
         try:
-            if self.tarefa == '' or tarefa is int or tarefa is float:
-                print("Não é possível adicionar uma tarefa vazia")
+            if self.tarefa == '' or isinstance(self.tarefa, (int, float)):
+                raise ValueError("Não é possível adicionar uma tarefa vazia")
+            
+            if TODO.addTarefa(self.tarefa):
+                print('Tarefa adicionada com sucesso')
             else:
-                if TODO.addTarefa(self.tarefa) == True:
-                    print('Tarefa adicionada com sucesso')
-                else:
-                    print('Erro ao adicionar tarefa')
+                print('Erro ao adicionar tarefa')
 
         except Exception as erro:
-            print("Ta errado man.")
-            return erro
+            print("Ocorreu um erro: ", erro)
 
 class Controllerremovertarefa():
     def __init__(self, excluir):
         self.excluir = excluir
+
+    def remover_tarefa(self):
         try:
-            if excluir is str or excluir is float:
-                print("Não é possivel excluir a tarefa")
-
-            else:
-                if TODO.removeTarefa(self.excluir) == True:
-                    print("Tarefa Excluida")
-
-                else:
-                    print("Algum problema foi encontrado")
-        except Exception as erro:
-            print("Ta errado man.")
-            return erro
+            if isinstance(self.excluir, (str, float)):
+                raise ValueError("Não é possível excluir a tarefa")
             
+            if TODO.removeTarefa(self.excluir):
+                print("Tarefa Excluída")
+            else:
+                print("Algum problema foi encontrado")
+        except Exception as erro:
+            print("Ocorreu um erro: ", erro)
 
 class Controllerlistartarefas():
-    def __init__(self):
-        controleLista = TODO.listaTarefas()
-        cont = 1
+    def listar_tarefas(self):
         try:
+            controleLista = TODO.listaTarefas()
+            cont = 1
             for i in controleLista:
                 print(f'{cont}° - {i}')
                 cont += 1
         except Exception as erro:
-            print("Ta errado man.")
-            return erro  
+            print("Ocorreu um erro: ", erro)
